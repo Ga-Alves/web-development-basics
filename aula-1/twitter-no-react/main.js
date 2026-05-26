fd236966cea9c368c253d8815de4e60dad746134
Ga-Alves <gabrielalvesreis@gmail.com>
2026-05-26 20:19:51 -0300

    refactor: simplify Feed class

--- aula-1/twitter-no-react/main.js
+++ aula-1/twitter-no-react/main.js
@@ -1,9 +1,8 @@
 class Feed {
-  posts = [];
+  posts = ["Neymar na copa!!"];
   nextId = 0;
 
   constructor() {
-    this.posts.push("Neymar na copa !!!");
   }
 
   addNewPost() {
@@ -20,17 +19,16 @@ class Feed {
     const feed = document.getElementById("feed");
     feed.innerHTML = "";
     this.posts.forEach((post, id) => {
-      feed.appendChild(this.createPost(post, id));
+      feed.appendChild(this.createPostHTMLElement(post, id));
     });
   }
 
-  createPost(content, id) {
+  createPostHTMLElement(content, id) {
     const newPostLi = document.createElement("li");
     newPostLi.setAttribute(
       "class",
       "bg-black text-white border border-gray-700 rounded-lg p-3 flex items-center justify-between gap-2",
     );
-    newPostLi.setAttribute("data-id", id);
 
     const postContentSpan = document.createElement("span");
     postContentSpan.setAttribute("class", "text-base");
